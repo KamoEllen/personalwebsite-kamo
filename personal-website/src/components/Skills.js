b5d6a6f705d3126390564c6c86332e3f30830480
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2023-01-15 02:34:09 +0200

    Update Skills.js

--- personal-website/src/components/Skills.js
+++ personal-website/src/components/Skills.js
@@ -34,8 +34,8 @@ export const Skills = () => {
             <div className="row">
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
-                        <h2>About Skills</h2>
-                        <p>I have learned different programming languages ​​so far and as far as my skills are concerned<br></br></p>
+                        <h2> Skills</h2>
+                        <p>I have learnt coding , I post someof my work online<br></br></p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
@@ -43,7 +43,7 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>English</h5>
+                                <h5>Generators</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
@@ -51,7 +51,7 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Bug Bounty</h5>
                             </div>
                         </Carousel>
                     </div>
